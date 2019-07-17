47c441021f7742f1114a802e8e60a8dc9d9404d7
Odin Aleksander Severinsen <odin@severinsen.no>
2019-07-17 18:41:18 +0200

    Finished KF library, remains to complete KF_node

--- src/wiimote_state_estimation/include/wiimote_state_estimation/kf.h
+++ src/wiimote_state_estimation/include/wiimote_state_estimation/kf.h
@@ -13,27 +13,72 @@ template <unsigned int NX, unsigned int NU, unsigned int NZ>
 class KalmanFilter
 {
 public:
+  /**
+ * Initialises the filter with the relevant matrices.
+ * @param x_pri_init Inital state
+ * @param S_init Initial covariance matrix
+ * @param A The A matrix in the linear state space model x_k+1 = Ax_k + Bu_k
+ * @param B The B matrix in the linear state space model x_k+1 = Ax_k + Bu_k
+ * @param C The C matrix in the linear observation model z_k = Cx_k
+ * @param R The covariance matrix of the process noise
+ * @param Q The covariance matrix of the measurement noise
+ */
   KalmanFilter(const Eigen::Matrix<double, NX, 1> &x_pri_init,
                const Eigen::Matrix<double, NX, NX> &S_init,
                const Eigen::Matrix<double, NX, NX> &A,
                const Eigen::Matrix<double, NX, NU> &B,
                const Eigen::Matrix<double, NZ, NX> &C,
-               const Eigen::Matrix<double, NX, NX> &R_init,
-               const Eigen::Matrix<double, NZ, NZ> &Q_init);
-  void iterate();
-  Eigen::Matrix<double, NX, 1> getState() const;
+               const Eigen::Matrix<double, NX, NX> &R,
+               const Eigen::Matrix<double, NZ, NZ> &Q);
+
+  /**
+   * Run one iteration of the Kalman Filter algorithm, given a current input and measurement.
+   * @param u Current input.
+   * @param z Current measurement.
+   */
+  void iterate(const Eigen::Matrix<double, NU, 1> &u, const Eigen::Matrix<double, NZ, 1> &z);
+
+  /**
+   * Get current a priori state.
+   * @return const Eigen::Matrix<double, NX, 1>& Constant reference to the internal x_pri.
+   */
+  const Eigen::Matrix<double, NX, 1> &getState_pri() const { return x_pri; }
+
+  /**
+   * Get current a priori covariance matrix.
+   * @return const Eigen::Matrix<double, NX, NX>& Constant reference to the internal S_pri.
+   */
+  const Eigen::Matrix<double, NX, NX> &getCoVar_pri() const { return S_pri; }
+
+  /**
+   * Get current a posteriori state.
+   * @return const Eigen::Matrix<double, NX, 1>& Constant reference to the internal x_post.
+   */
+  const Eigen::Matrix<double, NX, 1> &getState_post() const { return x_post; }
+
+  /**
+   * Get current a posteriori covariance matrix.
+   * @return const Eigen::Matrix<double, NX, 1>& Constant reference to the internal S_post.
+   */
+  const Eigen::Matrix<double, NX, NX> &getCoVar_post() const { return S_post; }
 
 private:
   Eigen::Matrix<double, NX, 1> x_pri;
   Eigen::Matrix<double, NX, 1> x_post;
-  Eigen::Matrix<double, NX, NX> S;
+  Eigen::Matrix<double, NX, NX> S_pri;
+  Eigen::Matrix<double, NX, NX> S_post;
   Eigen::Matrix<double, NX, NX> A;
   Eigen::Matrix<double, NX, NU> B;
   Eigen::Matrix<double, NZ, NX> C;
   Eigen::Matrix<double, NX, NZ> K;
   Eigen::Matrix<double, NX, NX> R;
   Eigen::Matrix<double, NZ, NZ> Q;
-  void update_x_pri();
+
+  void update_x_pri(const Eigen::Matrix<double, NU, 1> &u);
+  void update_x_post(const Eigen::Matrix<double, NZ, 1> &z);
+  void update_S_pri();
+  void update_S_post();
+  void update_K();
 };
 
 template <unsigned int NX, unsigned int NU, unsigned int NZ>
@@ -42,14 +87,54 @@ KalmanFilter<NX, NU, NZ>::KalmanFilter(const Eigen::Matrix<double, NX, 1> &x_pri
                                        const Eigen::Matrix<double, NX, NX> &A,
                                        const Eigen::Matrix<double, NX, NU> &B,
                                        const Eigen::Matrix<double, NZ, NX> &C,
-                                       const Eigen::Matrix<double, NX, NX> &R_init,
-                                       const Eigen::Matrix<double, NZ, NZ> &Q_init)
+                                       const Eigen::Matrix<double, NX, NX> &R,
+                                       const Eigen::Matrix<double, NZ, NZ> &Q)
     : x_pri(x_pri_init),
-      S(S_init),
+      S_pri(S_init),
       A(A),
       B(B),
       C(C),
-      R(R_init),
-      Q(Q_init) {}
+      R(R),
+      Q(Q) {}
+
+template <unsigned int NX, unsigned int NU, unsigned int NZ>
+void KalmanFilter<NX, NU, NZ>::update_x_pri(const Eigen::Matrix<double, NU, 1> &u)
+{
+  x_pri = A * x_post + B * u;
+}
+
+template <unsigned int NX, unsigned int NU, unsigned int NZ>
+void KalmanFilter<NX, NU, NZ>::update_x_post(const Eigen::Matrix<double, NZ, 1> &z)
+{
+  x_post = x_pri + K * (z - C * x_pri);
+}
+
+template <unsigned int NX, unsigned int NU, unsigned int NZ>
+void KalmanFilter<NX, NU, NZ>::update_S_pri()
+{
+  S_pri = A * S_post * A.transpose() + R;
+}
+
+template <unsigned int NX, unsigned int NU, unsigned int NZ>
+void KalmanFilter<NX, NU, NZ>::update_S_post()
+{
+  S_post = (Eigen::Matrix<double, NX, NX>::Identity() - K * C) * S_pri;
+}
+
+template <unsigned int NX, unsigned int NU, unsigned int NZ>
+void KalmanFilter<NX, NU, NZ>::update_K()
+{
+  K = S_pri * C.transpose() * (C * S_pri * C.transpose() + Q).inverse());
+}
+
+template <unsigned int NX, unsigned int NU, unsigned int NZ>
+void KalmanFilter<NX, NU, NZ>::iterate(const Eigen::Matrix<double, NU, 1> &u, const Eigen::Matrix<double, NZ, 1> &z)
+{
+  this->update_x_pri(u);
+  this->update_S_pri();
+  this->update_K();
+  this->update_x_post(z);
+  this->update_S_post();
+}
 
 #endif // WIIMOTE_STATE_ESTIMATION_KF_H
